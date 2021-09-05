2593e460399d1b0e679718a8d686ceaf2acffc4c
Nathaly <natharb09@gmail.com>
2021-09-04 21:25:56 -0500

    feat: setting up marvel api config

---
+++ marvel-store/src/services/api.ts
@@ -0,0 +1,31 @@
+import axios from 'axios'
+import md5 from 'md5'
+
+const marvelPublicKey = process.env.NEXT_PUBLIC_MARVEL_API_KEY
+const marvelPrivateKey = process.env.PRIVATE_MARVEL_API_KEY
+
+export const marvelApi = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_MARVEL_API_URL,
+  params: {
+    apikey: marvelPublicKey,
+    limit: 10
+  }
+})
+
+export const customApi = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL
+})
+
+export function serverSideConfig() {
+  const ts = new Date()
+  const md5Hash =
+    marvelPrivateKey && md5(ts + marvelPrivateKey + marvelPublicKey)
+
+  return {
+    params: {
+      ts,
+      hash: md5Hash,
+      offset: 0
+    }
+  }
+}
